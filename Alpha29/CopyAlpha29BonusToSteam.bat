979ec6f4b8c25c764d5541469fda9708558d48cf
Andrew Wright <aj268.wright@googlemail.com>
2019-09-06 20:44:45 +0100

    Merge with adventurer update.

--- Alpha29/CopyAlpha29BonusToSteam.bat
+++ Alpha29/CopyAlpha29BonusToSteam.bat
@@ -1,3 +1,3 @@
-xcopy "C:\KeeperRLMods\Alpha29\Alpha29Bonus\*.txt" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\data_free\game_config\A29Bonus_Dev\*.txt" /y
+xcopy "C:\KeeperRLMods\Alpha29\Alpha29Bonus\*.txt" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\data_free\game_config\Alpha29BonusDev\*.txt" /y
 
 pause
