d87ef83611b9462630733ba2a4e018358000b3a7
SoftMonster <aj268.wright@Googlemail.com>
2020-02-08 15:57:30 +0000

    Update batch file to deliver pngs

--- Alpha30/CopyAlpha30BonusToSteam.bat
+++ Alpha30/CopyAlpha30BonusToSteam.bat
@@ -1,3 +1,8 @@
-xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\*" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\*" /y
-
+xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\*"             "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\*" /y
+pause
+xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig16\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig16\*" /y
+pause
+xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig24\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig24\*" /y
+pause
+xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig30\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig30\*" /y
 pause
