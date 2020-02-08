e5e098c442124375b7b1d5b86fc70fa5a5fa2918
SoftMonster <aj268.wright@Googlemail.com>
2020-02-08 16:31:12 +0000

    Version info must update in reverse order.

--- Alpha30/CopyAlpha30BonusToSteam.bat
+++ Alpha30/CopyAlpha30BonusToSteam.bat
@@ -1,3 +1,5 @@
+xcopy "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\version_info" "C:\KeeperRLMods\Alpha30\Alpha30Bonus\version_info" /y
+pause
 xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\*"             "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\*" /y
 pause
 xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig16\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig16\*" /y
