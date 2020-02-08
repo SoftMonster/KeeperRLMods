fc321990c0309f89f57a474690658700b2505fe3
SoftMonster <aj268.wright@Googlemail.com>
2020-02-08 21:11:06 +0000

    Remove old files from mod before updating the mod

--- Alpha30/CopyAlpha30BonusToSteam.bat
+++ Alpha30/CopyAlpha30BonusToSteam.bat
@@ -1,10 +1,9 @@
 xcopy "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\version_info" "C:\KeeperRLMods\Alpha30\Alpha30Bonus\version_info" /y
-pause
+del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\*" /y
 xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\*"             "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\*" /y
-pause
+del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig16\*" /y
 xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig16\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig16\*" /y
-pause
+del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig24\*" /y
 xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig24\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig24\*" /y
-pause
+del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig30\*" /y
 xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig30\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig30\*" /y
-pause
