68a7f3a3830ced7a434aebaae3ec4248bd34b061
SoftMonster <aj268.wright@Googlemail.com>
2020-02-09 17:02:51 +0000

    Fix folder copy.

--- Alpha30/CopyAlpha30BonusToSteam.bat
+++ Alpha30/CopyAlpha30BonusToSteam.bat
@@ -1,9 +1,4 @@
 xcopy "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\version_info" "C:\KeeperRLMods\Alpha30\Alpha30Bonus\version_info" /y
-del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\*" /y
-xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\*"             "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\*" /y
-del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig16\*" /y
-xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig16\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig16\*" /y
-del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig24\*" /y
-xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig24\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig24\*" /y
-del "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig30\*" /y
-xcopy "C:\KeeperRLMods\Alpha30\Alpha30Bonus\orig30\*"      "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\orig30\*" /y
+RMDIR /S /Q "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus\"
+xcopy /s /i "C:\KeeperRLMods\Alpha30\Alpha30Bonus" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\Alpha30Bonus"
+pause
