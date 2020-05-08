5e534e0cbc8daebb9e287aa2f700c86ca1ba23c4
SoftMonster <aj268.wright@Googlemail.com>
2020-05-08 21:12:17 +0100

    Fix Axedio.

--- Alpha30/CopyFarmingModToSteam.bat
+++ Alpha30/CopyFarmingModToSteam.bat
@@ -1,3 +1,3 @@
-xcopy "C:\KeeperRLMods\Alpha30\FarmingMod\*" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\FarmingMod\*" /y
+xcopy /s /i "C:\KeeperRLMods\Alpha30\FarmingMod\*" "C:\Program Files (x86)\Steam\steamapps\common\KeeperRL\mods\FarmingMod\*" /y
 
 pause
